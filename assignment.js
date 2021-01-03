b7f7f738aa1624ceb46ca900a04b0a1fc0cd50df
Gopal <gopal.jivan@gmail.com>
2021-01-03 11:52:53 -0500

    Adding first lesson solution

---
+++ assignment.js
@@ -0,0 +1,10 @@
+let userInput = 5;
+let result;
+
+result = 18 + userInput;
+result = result -1;
+result = result * 2;
+result = result /3;
+
+alert ('User Input '+ userInput);
+alert ('Result ' + result);
