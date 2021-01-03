1e79054b2347d59f5719ac177610b9e17b2d4912
Gopal <gopal.jivan@gmail.com>
2021-01-03 13:35:19 -0500

    Formatting and moving function to the bottom

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,17 +1,13 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-function addCalc(num1, num2) {
-  const result = num1 + num2;
-  return result;
-}
-
-
-currentResult = addCalc(1,2);
-
-
+currentResult = addCalc(1, 2);
 
 let calculationDescription = '(' + defaultResult + ') + 10 + 0';
 
 outputResult(currentResult, calculationDescription);
 
+function addCalc(num1, num2) {
+  const result = num1 + num2;
+  return result;
+}
