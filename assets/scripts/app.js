827dca48da593ed9096b48fd689b918eb9c85a79
Gopal <gopal.jivan@gmail.com>
2021-01-03 11:43:25 -0500

    Adding string manipulation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,9 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
-currentResult = currentResult + 10+0;
+currentResult = currentResult + 10 + 0; 
 
-let calculationDescription = '(' + defaultResult + ') + 10 + 0'
+let calculationDescription = '(' + defaultResult + ') + 10 + 0';
+let errorMessage = 'An error \n' + 'occured!';
 
-outputResult(currentResult,calculationDescription);
+outputResult(currentResult, errorMessage);
